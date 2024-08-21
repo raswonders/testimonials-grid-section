ff97c8fa2f75e82aca5d7b039b0d15671708803b
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-08-21 14:49:34 +0200

    add responsiveness for desktop

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import data from "./data/testimonials.json";
 
 function App() {
   return (
-    <>
-      <main className="mx-6 my-20 grid gap-6">
+    <div className="lg:min-h-screen lg:grid lg:place-content-center">
+      <main className="max-w-[69.375rem] mx-6 my-20 grid gap-6 lg:gap-[1.875rem] lg:grid-rows-2 lg:grid-cols-4">
         {data.testimonials.map((entry, index) => (
           <Testimonial entry={entry} index={index} key={entry.name} />
         ))}
       </main>
-    </>
+    </div>
   );
 }
 
--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -5,18 +5,18 @@ interface Props {
   index: number;
 }
 
-const testimonialColor = [
-  "text-white bg-[#733FC8]",
+const testimonialStyle = [
+  "text-white bg-[#733FC8] lg:col-span-2",
   "text-white bg-dark-gray",
   "text-dark-gray bg-white",
-  "text-white bg-[#19202D]",
-  "text-white bg-dark-gray",
+  "text-white bg-[#19202D] lg:col-span-2",
+  "text-dark-gray bg-white lg:row-span-2 lg:row-start-1 lg:col-start-4",
 ];
 
 export function Testimonial({ entry, index }: Props) {
   return (
     <article
-      className={`p-8 -z-20 relative flex flex-col gap-4 rounded-lg shadow-[40px_60px_50px_-47px_rgba(72,85,106,0.25)] ${testimonialColor[index % testimonialColor.length]}`}
+      className={`p-8 -z-20 relative flex flex-col gap-4 rounded-lg shadow-[40px_60px_50px_-47px_rgba(72,85,106,0.25)] ${testimonialStyle[index % testimonialStyle.length]}`}
     >
       {index === 0 && (
         <img
@@ -24,16 +24,16 @@ export function Testimonial({ entry, index }: Props) {
           alt=""
           width="104"
           height="102"
-          className="absolute top-0 right-6 -z-10"
+          className="absolute top-0 right-6 lg:right-20 -z-10"
         />
       )}
       <div className="flex space-x-4 items-center">
         <img
           src={entry.image_url}
           alt={entry.image_alt}
-          className="w-7 h-7 rounded-full border-2 border-gray-500"
+          className="w-8 h-8 rounded-full border-2 border-gray-500"
         />
-        <div className="flex flex-col gap-1">
+        <div>
           <h2 className="font-medium text-sm">{entry.name}</h2>
           <h3 className="font-medium text-xs opacity-50">{entry.status}</h3>
         </div>
